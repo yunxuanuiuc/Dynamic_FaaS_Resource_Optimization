6f7db935e1224b2c04b4559a0acc36d9e5f769b5
Hank Zhang <hz97587@gmail.com>
2023-10-29 13:03:07 -0500

    updated Log Processor

--- LogProcessor/LogProcessor.py
+++ LogProcessor/LogProcessor.py
@@ -1,36 +1,28 @@
-import boto3
+import base64
+import gzip
+import json
 import re
-import time
 
 def lambda_handler(event, context):
-    
-    startTimeStamp = 0
-    client = boto3.client('logs')
+    # Decode and decompress the log data
+    compressed_payload = base64.b64decode(event['awslogs']['data'])
+    uncompressed_payload = gzip.decompress(compressed_payload)
 
-    while (True):
+    # Convert log data from JSON into a Python dictionary
+    log_data = json.loads(uncompressed_payload)
+
+    # Now you can access the log events as a Python list
+    log_events = log_data['logEvents']
+
+    # Iterate over each log event and print its content
+    for log_event in log_events:
+        log_line = log_event['message']
+        request_id = re.search(r'RequestId: (.*?) ', log_line).group(1)
+        duration = re.search(r'Duration: (.*?) ms', log_line).group(1)
+        memory_size = re.search(r'Memory Size: (.*?) MB', log_line).group(1)
+        max_memory_used = re.search(r'Max Memory Used: (.*?) MB', log_line).group(1)
         
-        try:
-            response = client.filter_log_events(
-                logGroupName = '/aws/lambda/CpuIntensiveFunction',
-                startTime = startTimeStamp + 1
-            )
-            
-            for event in response['events']:
-                if not event['message'].startswith('REPORT RequestId'):
-                    continue
-                
-                log_line = event['message']
-                
-                request_id = re.search(r'RequestId: (.*?) ', log_line).group(1)
-                duration = re.search(r'Duration: (.*?) ms', log_line).group(1)
-                memory_size = re.search(r'Memory Size: (.*?) MB', log_line).group(1)
-                max_memory_used = re.search(r'Max Memory Used: (.*?) MB', log_line).group(1)
-                
-                print(f"RequestId: {request_id}")
-                print(f"Duration: {duration} ms")
-                print(f"Memory Size: {memory_size} MB")
-                print(f"Max Memory Used: {max_memory_used} MB")
-                    
-                startTimeStamp = event['timestamp']
-        except Exception as error:
-            print(error)
+        print(f"RequestId: {request_id}")
+        print(f"Duration: {duration} ms")
+        print(f"Memory Size: {memory_size} MB")
+        print(f"Max Memory Used: {max_memory_used} MB")
