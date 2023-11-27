9a7e829a807b19c135c81b817163ce0bb65ba653
Cesar Arevalo <cesar@cesararevalo.com>
2023-11-26 20:50:40 -0600

    - Adding a way to send the requests to the function asynchronously, without waiting for the response. - Added a configuration option to the load generator for the above, the config is mode and the options are 'sync' or 'async'.

--- dfaastest/gen_load.py
+++ dfaastest/gen_load.py
@@ -3,6 +3,7 @@ import json
 import time
 from datetime import datetime
 from importlib import import_module
+from multiprocessing import Process
 
 import requests
 
@@ -19,6 +20,7 @@ class GenLoad(object):
                  dryrun=False,
                  duration=0,
                  funk_config=None,
+                 mode=None,
                  ):
         self.debug = debug
         self.funk_name = funk_name
@@ -30,6 +32,7 @@ class GenLoad(object):
         self.api = get_rest_api(funk_config['api_name'])
         self.api_url = construct_api_url(self.api['id'], get_region_name(), funk_config['api_stage'], funk_config['api_base_path'])
         self.funk_module = import_module(funk_config['module'])
+        self.mode = mode
 
     def send_request(self, api, payload):
         if not self.dryrun:
@@ -54,6 +57,11 @@ class GenLoad(object):
         self.send_request(api=self.api_url, payload=payload)
 
     def run(self):
+        match self.mode:
+            case 'sync': self.run_sync()
+            case 'async': self.run_async()
+
+    def run_sync(self):
         start_time = datetime.utcnow()
         print(f'Starting to run - start time: {start_time}...')
 
@@ -73,9 +81,44 @@ class GenLoad(object):
                             time.sleep(wait_period['wait'])
 
                     except Exception as e:
-                        print(f'Exception ocurred while sending request: {e}')
+                        print(f'Exception occurred while sending request: {e}')
 
         print(f'Ended the run.')
+
+    def run_async(self):
+        threads = []
+
+        start_time = end_time = datetime.utcnow()
+        print(f'Starting the async run - start time: {start_time}...')
+
+        # Will need to kill the process or send it the kill signal
+        while (end_time - start_time).total_seconds() < self.duration:
+
+            # for each of the wait periods
+            for wait_period in self.wait_periods:
+                period_start_time = datetime.utcnow()
+
+                while (datetime.utcnow() - period_start_time).total_seconds() < wait_period['duration']:
+                    try:
+
+                        worker_thread = Process(target=self.send_workload_request(), args=(), daemon=True)
+                        worker_thread.start()
+
+                        threads.append(worker_thread)
+
+                        if wait_period['wait'] > 0:
+                            time.sleep(wait_period['wait'])
+
+                    except Exception as e:
+                        print(f'Exception occurred while sending request: {e}')
+
+            end_time = datetime.utcnow()
+
+        # Make sure the threads complete
+        for worker_thread in threads:
+            worker_thread.join()
+
+        print(f'Ended the async run - end time: {end_time} - total function runtime duration: {(datetime.utcnow() - start_time).total_seconds()}')
 
     def test(self):
         print(f'Starting test...')
@@ -115,6 +158,7 @@ if __name__ == '__main__':
         duration=load_gen_config['duration'],
         wait_periods=load_gen_config['wait_periods'],
         funk_config=funktions_config[funk_name],
+        mode=load_gen_config['mode'],
     )
 
     match args.action:
